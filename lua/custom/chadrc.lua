04d924474034810b2a3d97aba9ef6609a9a0c29d
Vitaly Shevchenko <blackjack358@gmail.com>
2024-01-07 19:36:54 +0300

    NvChad: move from version 1.0 to 2.0

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,17 +1,6 @@
+---@type ChadrcConfig
 local M = {}
 
-M.plugins = require "custom.plugins"
-
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
-M.ui = {
-  -- theme stuff
-  theme = "gruvchad",
-}
-
-M.mappings = require "custom.mappings"
-
--- Fix spelling errors
-vim.cmd('iabbrev cosnt const')
+M.ui = { theme = 'onedark' }
 
 return M
